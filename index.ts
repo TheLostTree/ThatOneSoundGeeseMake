ec8fe8567d08feb76ace922d309214bd8788e4b0
TheLostTree <65834918+TheLostTree@users.noreply.github.com>
2023-08-05 12:44:38 -0700

    iridium lol

--- index.ts
+++ index.ts
@@ -11,12 +11,26 @@ import CmdIds from "./CmdIds.json"
 
 const root = pbjs.loadSync("protos/StarRail.proto");
 
+import WebSocket, {WebSocketServer} from "ws";
 
-function stringify(...obj: any){
-    return JSON.stringify(obj, (key, value) =>
-    { return typeof value === 'bigint'
-    ? value.toString()
-    : value instanceof Long ? value.toString() : value}, 2);
+
+
+function stringify(...args: any){
+
+    let obj = args[0];
+
+    return JSON.stringify(obj, (key, value) =>{
+        if(value instanceof Long){
+            return value.toString();
+        }
+        if(value instanceof pbjs.util.LongBits){
+
+        }
+        if(typeof value === "bigint"){
+            return value.toString();
+        }
+        return value;
+    }, 2);
 }
 
 function EmVecToArr<T>(vec: Vector<T>): T[] {
@@ -138,23 +152,45 @@ function parseKcpPacket(buf: Buffer){
             //i give up :pensive:
             process.exit(0);
 
+        }else{
+            // trying new key
+            xored = xor(buf, curKey);
+            magic = xored.readUInt32BE(0);
+            if(magic != startMagic){
+                console.log("still no magic");
+                process.exit(0);
+            }
+
         }
         
-    }else{
-        let cmdId = xored.readUInt16BE(4);
-        let packetHeaderLen = xored.readUInt16BE(6);
-        let dataLen = xored.readUInt32BE(8);
+    }
 
-        if(packetHeaderLen + dataLen + 16 != xored.length){
-            // console.log("%s, %s",packetHeaderLen + dataLen + 16 , xored.length);
-        }
-        let dataSlice = xored.subarray(12 + packetHeaderLen, 12 + packetHeaderLen + dataLen);
+    let cmdId = xored.readUInt16BE(4);
+    let packetHeaderLen = xored.readUInt16BE(6);
+    let dataLen = xored.readUInt32BE(8);
 
-        let obj = parseProto(cmdId, dataSlice);
+    
+    let dataSlice = xored.subarray(12 + packetHeaderLen, 12 + packetHeaderLen + dataLen);
 
-        console.log(`cmdId: ${CmdIds[cmdId]}, obj: ${stringify(obj)}`)
-        lastPack = obj;
-    };
+    let magic2 = xored.readUInt32BE(12 + packetHeaderLen + dataLen);
+    if(packetHeaderLen + dataLen + 16 != xored.length){
+        console.log("weird packet length discrepancy? reported: %s, actual: %s",packetHeaderLen + dataLen + 16 , xored.length);
+    }
+
+    if(magic2 != endMagic){
+        console.log(`magic2 was ${magic2}`)
+        process.exit(0);
+    }
+
+    let obj = parseProto(cmdId, dataSlice);
+
+    lastPack = obj;
+
+    return {
+        cmdId,
+        data: dataSlice,
+        obj
+    }
 }
 
 
@@ -173,6 +209,7 @@ function parseProto(cmdId: number, buf: Buffer){
             longs: Long,
             enums: String,
             bytes: String,
+            defaults: true,
         });
         return obj;
     }else{
@@ -227,30 +264,46 @@ function parseProtoIsh(buf:Buffer){
 }
 
 
-function getFrames(){
-    let frames = JSON.parse(fs.readFileSync("frames.json", "utf8"));
-
-
-    return frames.map((x: any) =>Buffer.from(x.data_sources[0].data, "base64"));
-}
 
 
 
 
 
+type TrafficPacket = {
+    cmdId: number,
+    data: Buffer,
+    seq: number,
+    obj: any,
+    source: "client" | "server"
+};
+class TrafficInstance{
+    constructor(public callback: (pack: TrafficPacket)=>void){}
+    clientKcp : Kcp = null!;
+    serverKcp : Kcp = null!;
+
+    clientPort = 0;
 
 
+    seq = 0;
 
-async function main1() {
-
-    //if you run a websocket or something with an actual libpcap handle, you can just replace this and modify it a teensy bit :D
-    const frames : Buffer[] = getFrames();
-    let clientKcp : Kcp = null!;
-    let serverKcp : Kcp = null!;
-
-    let clientPort = 0;
-    while(frames.length > 0) {
-        let frame = frames.shift()!;
+    kcpRecv(kcp: Kcp){
+        let size = kcp.peekSize();
+        if(size <= 0) return {
+            success: false
+        };
+        let buf = Buffer.alloc(size);
+        let read = kcp.recv(buf);
+        if(read != size) return {
+            success: false
+        };
+        // buf is a full packet
+        return {
+            buf,
+            success: true
+        };
+    }
+    
+    processPacket(frame: Buffer){
         let data = frame;
 
         //cut off 34 bytes of header
@@ -269,42 +322,135 @@ async function main1() {
             switch (magic) {
                 case 0xFF:
                     //connect 
-                    clientPort = sourceport;
+                    this.clientPort = sourceport;
                     console.log("client connect handshake")
                     break;
                 case 0x145:
-                    clientKcp = new Kcp(conv, token, ()=>{});
-                    serverKcp = new Kcp(conv, token, ()=>{});
+                    this.clientKcp = new Kcp(conv, token, ()=>{});
+                    this.serverKcp = new Kcp(conv, token, ()=>{});
                     console.log("kcp created")
                     break;
                 case 0x194:
                     break;
             }
         }else{
-            clientPort == sourceport ? clientKcp.input(data) : serverKcp.input(data);
-
-            for(let kcp of [clientKcp, serverKcp]){
-                while (true) {
-                    let size = kcp.peekSize();
-                    if(size <= 0) break;
-                    let buf = Buffer.alloc(size);
-                    let read = kcp.recv(buf);
-                    if(read != size) break;
-                    // buf is a full packet
-    
-                    parseKcpPacket(buf);
-                    
-                } 
+            this.clientPort == sourceport ? this.clientKcp.input(data) : this.serverKcp.input(data);
+            let shouldContinue = true;
+            while(shouldContinue){
+                let serverRecv = this.kcpRecv(this.serverKcp);
+                let clientRecv = this.kcpRecv(this.clientKcp);
+                if(!serverRecv.success && !clientRecv.success){
+                    shouldContinue = false;
+                    break;
+                }
+                if(serverRecv.success){
+                    let ret = <TrafficPacket>parseKcpPacket(serverRecv.buf);
+                    ret.seq = this.seq++;
+                    ret.source = "server";
+                    this.callback(ret);
+                }
+                if(clientRecv.success){
+                    let ret = <TrafficPacket>parseKcpPacket(clientRecv.buf);
+                    ret.seq = this.seq++;
+                    ret.source = "client";
+                    this.callback(ret);
+                }
             }
            
         }
     }
-
-
-
-
-
 }
 
+
+
+
+async function main1() {
+
+    //if you run a websocket or something with an actual libpcap handle, you can just replace this and modify it a teensy bit :D
+    const frames : Buffer[] = (() => {
+        let f = JSON.parse(fs.readFileSync("frames.json", "utf8"));
+        return f.map((x: any) =>Buffer.from(x.data_sources[0].data, "base64"));
+    })()
+
+    let traffic = new TrafficInstance((obj:TrafficPacket)=>{
+        console.log(`cmdId: ${CmdIds[obj.cmdId]}, obj: ${stringify(obj.obj)}`)
+    });
+    for(let frame of frames){
+        traffic.processPacket(frame);
+    }
+}
+
+//smh no typedefs
+//@ts-ignore 
+import * as cap from "cap";
+
+
+
+async function main2() {
+    let c = new cap.Cap();
+    let device = cap.Cap.findDevice("192.168.11.143");
+    let filter = 'udp port 23301';
+    let buffer = Buffer.alloc(65535);
+
+    let linkType = c.open(device, filter, 10 * 1024 * 1024, buffer);
+    c.setMinBytes && c.setMinBytes(0);
+
+
+    let ws = new WebSocketServer({
+        //'ws://127.0.0.1:40510'
+        port: 40510
+    })
+
+
+    ws.on('connection', (ws)=>{
+        console.log("new connection");
+    })
+
+
+    //ping
+    //move sync
+    let ignored = ["PlayerHeartBeatScRsp", "PlayerHeartBeatCsReq", "SceneEntityMoveCsReq", "SceneEntityMoveScRsp"]
+
+    let traffic = new TrafficInstance((obj:TrafficPacket)=>{
+        console.log(`seq: ${obj.seq.toString().padStart(5)}, cmdId: ${CmdIds[obj.cmdId]}`)
+
+        if(CmdIds[obj.cmdId] && ignored.includes(CmdIds[obj.cmdId])){
+            return;
+        }
+        let iridiumObj = {
+            cmd : "PacketNotify",
+            data: [
+                {
+                    packetID : obj.cmdId,
+                    protoName: CmdIds[obj.cmdId],
+                    object: obj.obj,
+                    packet: obj.data.toString("base64"),
+                    source: obj.source == "client" ? 1 : 0,
+                }
+            ]
+        }
+        ws.clients.forEach(x=>{
+            x.send(stringify(iridiumObj));
+        });
+
+        
+
+    });
+
+    c.on('packet', function(nbytes: number, trunc: boolean) {
+        if (linkType === 'ETHERNET') {
+            //clone buf
+            let buf = Buffer.alloc(nbytes);
+            buffer.copy(buf, 0, 0, nbytes);
+
+            traffic.processPacket(buf);
+        } else{
+            console.log('Non-ethernet packet?');
+            console.log(linkType)
+        }
+    })
+}
+
+// main2();
 
 main1();
